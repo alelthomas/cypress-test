599a3ab946ed5fa0df7e7ea437f59d063b394019
Ale <93217218+alelthomas@users.noreply.github.com>
2022-10-05 19:01:26 -0400

    fix config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // e2e options here
+      // implement node event listeners here
     },
   },
 });
